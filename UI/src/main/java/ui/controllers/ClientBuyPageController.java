78ee7f48ff0f959e8c3efb9138dad5a79844e0fd
TudorStanca <vlad.stanca@stud.ubbcluj.ro>
2025-05-24 15:04:46 +0300

    A commit with small changes

--- UI/src/main/java/ui/controllers/ClientBuyPageController.java
+++ UI/src/main/java/ui/controllers/ClientBuyPageController.java
@@ -1,10 +1,16 @@
 package ui.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import model.dto.ClientDTO;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import services.IServices;
+import ui.MainApplication;
+import ui.View;
+import ui.utils.ObserverManager;
 
 public class ClientBuyPageController implements IController {
 
@@ -18,5 +24,24 @@ public class ClientBuyPageController implements IController {
         this.service = service;
         this.stage = stage;
         this.user = user;
+    }
+
+    @FXML
+    private void handleBack(){
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource(View.CLIENT_MAIN.path));
+            fxmlLoader.setControllerFactory(c -> new ClientMainPageController(service, stage, user));
+
+            Pane root = fxmlLoader.load();
+            stage.getScene().setRoot(root);
+
+            root.requestFocus();
+            stage.setResizable(false);
+            stage.setTitle(View.CLIENT_MAIN.title);
+
+            ObserverManager.getInstance().setCurrentController(fxmlLoader.getController());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
